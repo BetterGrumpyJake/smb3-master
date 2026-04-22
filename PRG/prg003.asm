c02c5bf7a989f612f136a655525a2c7cb336a57b
BetterGrumpyJake <139168180+BetterGrumpyJake@users.noreply.github.com>
2026-04-21 20:13:13 -0800

    Update prg003.asm

--- PRG/prg003.asm
+++ PRG/prg003.asm
@@ -1829,6 +1829,8 @@ Object_DoMoveBounceConveyor:
 	BNE PRG003_A946	 ; If this isn't a Boom Boom ball, jump to PRG003_A946
 
 	LDA <Objects_Y,X
+	;j
+	;CMP #$70
 	CMP #$00
 	BLT ObjInit_BoomBoomQBall ; If ball is higher than $70, jump to ObjInit_BoomBoomQBall
 
